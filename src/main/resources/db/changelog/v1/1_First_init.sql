89c1b8fbdfb3037fcd62884c2b269c4ea439c226
Aleksandr Moskalenkov <amk.upsale@gmail.com>
2020-04-21 00:45:23 +0300

    AddDataBase

---
+++ src/main/resources/db/changelog/v1/1_First_init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `trustmeenglish`.`users` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
